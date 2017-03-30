10e3729b7c5f00224b30047a6319f401e62f1787
aptrishu <code.rishus23@gmail.com>
2017-03-31 01:26:23 +0530

    Initial commit

---
+++ prog1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()    
+{
+	//coala is a static code analyzer tool we can use it in our codebase. It's cool.
+	printf("%s\n","hello world");       
+	return 0;
+}
---
+++ prog2.py
@@ -0,0 +1,10 @@
+import os, sys
+def checkcoala():
+	""" It is a dummy method. """
+	a = 'valid string'
+	b = "again valid string"
+	print(a+b+"another addition to the set of valid strings, wow")
+	c = True
+	if c:
+		if 'valid' in a or 'valid' in b or len(a) + len(b) > 9 or 'again' in valid:
+			     print('Its valid')
